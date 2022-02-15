53fecfa266616871a1bdf243fcb4a8da2719405a
sadat.malik <sadat.malik@me.com>
2022-02-15 22:01:28 +0000

    Adding Fabric8 plugin and supporting config

---
+++ src/main/java/com/sadatmalik/masterworksbom/MsMasterWorksBomApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.masterworksbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMasterWorksBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMasterWorksBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadatmalik/masterworksbom/MsMasterWorksBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.masterworksbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMasterWorksBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
